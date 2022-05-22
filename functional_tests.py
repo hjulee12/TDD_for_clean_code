882dfd8e397feb8eff6ab281387821185b0f679b
Hyeonju <hjulee12@gmail.com>
2022-05-22 21:40:54 +0900

    템플릿을 이용하도록 홈페이지 뷰 리팩터링

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,21 +16,37 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 웹 사이트를 확인하러 간다.
         self.browser.get('http://localhost:8000')
 
-        # 웹페이지 타이틀과 헤더가 'To-do'를 표시하고 있다.
+        # 웹페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-        assert 'To-do' in browser.title
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
+        # self.fail('Finish the test!')
+        # assert 'To-do' in browser.title
 
         # 그녀는 바로 작업을 추가하기로 한다.
+        inputbox = self.browser.find_element(by=By.Id, value='id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
 
         # "공작깃털 사기"라고 텍스트 상자에 입력한다
         # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element(by=By.id, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물만들기"라고 입력한다(에디스는 매우 체계적인 사람이다)
+        self.fail('Finish the test!')
 
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
         # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
